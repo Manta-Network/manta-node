c018f6a7f77f804c1f88b971f35c710708e211d8
Shumo Chu <shumo.chu@gmail.com>
2021-02-02 17:46:31 -0800

    wip client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,21 @@
 use pallet_manta_dap::crypto_types::*;
+use pallet_manta_dap::priv_coin::*;
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 
 fn main() {
+   let args = Cli::from_args();
+
+    
     println!("Hello, world!");
 }
